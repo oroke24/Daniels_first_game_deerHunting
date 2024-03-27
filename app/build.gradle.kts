9620d5361329a1b3242bf278dbd4999e2e447f90
Jamie (JT) O'Roke <56612513+oroke24@users.noreply.github.com>
2024-03-27 04:09:13 -0700

    updated list of users to be stored locally and sorted by high score.  Also changed backgrounds and change size of deer when clicked

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.games:games-activity:1.2.2")
+    implementation("com.google.firebase:firebase-crashlytics-buildtools:2.9.9")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    //GSON
+    implementation ("com.google.code.gson:gson:2.10.1")
 }
